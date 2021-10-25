ed30b624630c91efc38828ce5ca77b9964149cb1
Aaron Chan <jofaduh123@outlook.com>
2021-10-25 12:24:47 -0400

    Redo/Undo, Prefab Spawns

---
+++ Assets/Scripts/Command Pattern/InputPlane.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputPlane : MonoBehaviour
+{
+    Camera maincam;
+    RaycastHit hitInfo;
+    public Transform cubePrefab;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        maincam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = maincam.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hitInfo, Mathf.Infinity))
+            {
+                Color c = new Color(Random.Range(0.5f, 1f), Random.Range(0.5f, 1f), Random.Range(0.5f, 1f));
+                //CubePlacer.PlaceCube(hitInfo.point, c, cubePrefab);
+
+                ICommand command = new PlaceCubeCommand(hitInfo.point, c, cubePrefab);
+                CommandInvoker.AddCopmmand(command);
+            }
+        }
+        
+    }
+}
